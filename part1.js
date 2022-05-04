67752438a26af9ef4b9710977d125f343159bf68
Jazzmin Smith <jazzmin.smith01@gmail.com>
2022-05-04 12:33:59 -0500

    add readme

--- part1.js
+++ part1.js
@@ -6,19 +6,12 @@ const location = {
     isShip:true,//come back to change the boolean
     hit:true //come back to change the boolean
 }
-let isGridFilled = false;
 
-//grid
+const grid = [
+    [],[],[],[],[],[],[],[],[],[]
+];
 
-const grid = [[],[],[],[],[],[],[],[],[],[]];
 
-for(let i = 0; i < grid.length; i++){
-    if(i == grid.length){
-        isGridFilled = true
-        
-    }else{
-        while(!isGridFilled){
-            console.log(grid.push(location));
-        }
-    }
-}
+
+
+
