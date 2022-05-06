53362a48b47af04a031a4afb6cdf0780f341216e
Jazzmin720 <97210818+Jazzmin720@users.noreply.github.com>
2022-05-06 06:35:47 -0500

    add grid logic via upload

--- part1.js
+++ part1.js
@@ -1,6 +1,8 @@
 var readlineSync = require('readline-sync');
 
 const pressAnyKey = readlineSync.question('Press any key to start the game.');
+//const strike = readlineSync.question('Enter a location to strike.');
+//const enterBoardSize = readlineSync.question('Enter the board size(3x3),(5x5),(10x10)');
 
 
 
@@ -10,14 +12,24 @@ let locationTwo = Math.floor(Math.random()*66 );
 
 //create grid
 let grid = [];
+let alphabet = 'abcd'.toUpperCase();
 
 
-  function createGrid(size){  
-      for(let i = 0; i < size; i ++){
-        grid[i] = []
-        console.log(grid[i]);
+
+ const createGrid = (size) => { 
+      let r;
+      let c = 0; 
+      for(r = 0; r < size; r ++){
+        grid[r] = [];
+        for (c = 0; c < size; c ++){
+             grid[r][c] = `${alphabet[r]}${c + 1}`;
+        }
     }
-  }
+  };
+
+  //strike
+
+
 
 /*function randomInd (){
     let i = 0
@@ -33,8 +45,9 @@ let ships = [
     {location:, hits:},
     {location:, hits:}
 ]*/
+createGrid(alphabet.length);
+console.table(grid);
 
-createGrid(9);
 
 
 
