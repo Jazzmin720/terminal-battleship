d62c7e5a8fb99df862103eae3512306c520d17ff
Jazzmin Smith <jazzmin.smith01@gmail.com>
2022-05-10 11:09:13 -0500

    add nested function to repeat strike

--- part1.js
+++ part1.js
@@ -1,6 +1,6 @@
 var readlineSync = require('readline-sync');
 
-const pressAnyKey = readlineSync.question('Press any key to start the game.'); //CAN ONLY PRESS ENTER?
+ //CAN ONLY PRESS ENTER?
 
 let isIndex = false;
 
@@ -11,6 +11,7 @@ let struckShips = [];
 //create grid
 let grid = [];
 let alphabet = 'abcd'.toUpperCase();
+const pressAnyKey = readlineSync.question('Press any key to start the game.');
 
 const createGrid = (size) => { 
       let r;
@@ -24,6 +25,7 @@ const createGrid = (size) => {
       console.table(grid);
 };
 createGrid(alphabet.length);
+
   
 //place ship in random index and strike
 const randomIndex = () => {
@@ -36,38 +38,50 @@ const randomIndex = () => {
  
     
     if(randomIndexOne || randomIndexTwo){
-      let gridIndex = grid[r][c]
+      let gridIndex = grid[r][c]      //remove this from the func
       let gridIndexTwo = grid[s][d]
       console.log(grid[r][c],grid[s][d])
     }
-    let strike = readlineSync.question('Enter a location to strike.');
     
-  while(!isIndex){
-    if(strike == grid[r][c] || strike == grid[s][d]){
-      console.log('Hit!')
-      readlineSync.question('Enter a location to strike.')
-    }else if(strike !== grid[r][c] || strike !== grid[s][d]){
+    const strike = () => {
+      const enterLocation = readlineSync.question('Enter a location to strike.')
       while(!isIndex){
-       console.log('Miss...')
-       let strikeAgain = readlineSync.question('Enter a location to strike.')
-        if(strikeAgain == grid[r][c] || strikeAgain == grid[s][d]){
-        console.log('Hit! You have sunk a battleship. 1 remaining')//make this dynamic by splicing
-        readlineSync.question('Enter a location to strike.')
-        //isIndex = true
+        if(enterLocation == grid[r][c] || enterLocation == grid[s][d]){
+            console.log('Hit!')
+            strike();
+            
+          }else if(strike !== grid[r][c] || strike !== grid[s][d]){
+            while(!isIndex){
+             console.log('Miss...')
+             strike();
+             
+            
+            }
         }
-      
       }
     }
+  strike();
   }
-};
+
+  randomIndex();
+    
+
+
+
+
+  
+    
+    
+  
+
 
            
-      
+      //ADD THE DELETED SHIPS AND 'YOU'VE ALREADY PICKED THIS LOCATION
      
         
         
     
-  randomIndex();
+ 
  
   
 
@@ -79,6 +93,27 @@ const randomIndex = () => {
 
 
 
+
+
+
+
+
+
+ 
+  
+
+
+ 
+
+ 
+
+
+
+  /*let deletedShips = delete grid[strike]
+  struckShips.push(deletedShips)
+  console.log(struckShips)
+  console.log('Miss...')
+  let strikeAgain = readlineSync.question('Enter a location to strike.')*/
 
 
 
